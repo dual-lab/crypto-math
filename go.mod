18c012e5f71ce115161e886cdb8eee4bda09f83b
dmike <cipmiky@gmail.com>
2021-01-20 22:33:28 +0100

    chore: change lib and app name

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module com.github/dual-lab/crypto-math-cli
+module com.github/dual-lab/crypto-math
 
 go 1.15
+
+require github.com/spf13/cobra v1.1.1
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"com.github/dual-lab/crypto-math/m/cmd"
+)
 
 func main() {
-	fmt.Println("Hello main go")
+	cmd.Excecute()
 }
